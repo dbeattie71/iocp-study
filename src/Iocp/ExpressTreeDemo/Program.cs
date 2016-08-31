2e6d9fddbf7a673bbb3f966eb4ef29e3f4aec1c6
sujunxuan <suinsomnia@gmail.com>
2016-08-31 21:09:38 +0800

    express

--- src/Iocp/ExpressTreeDemo/Program.cs
+++ src/Iocp/ExpressTreeDemo/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace ExpressTreeDemo
 {
@@ -10,6 +7,25 @@ namespace ExpressTreeDemo
     {
         static void Main(string[] args)
         {
+            var mymath = new MyMath();
+            var add = typeof(MyMath).GetMethod("Add");
+            var math = Expression.Parameter(typeof(MyMath));
+            var a = Expression.Parameter(typeof(int), "a");
+            var b = Expression.Parameter(typeof(int), "b");
+            var body = Expression.Call(math, add, a, b);
+            var lambda = Expression.Lambda<Func<MyMath, int, int, int>>(body, math, a, b);
+
+            var addFunc = lambda.Compile();
+            Console.WriteLine(addFunc(mymath, 1, 2));
+            Console.Read();
+        }
+    }
+
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
